0eb7065a3aeb3ce91a7d89961fa357f720c8462b
Dhrutingami1226 <DHRUTINGAMI@GMAIL.COM>
2026-02-15 11:25:33 +0530

    Delete backend directory

--- backend/routes/logout.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { logoutUser } from "../Controller/logout.mjs";
-
-const router = express.Router();
-
-// POST /api/logout
-router.post("/", logoutUser);
-
-export default router;
